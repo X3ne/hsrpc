fa7820b744d130c73bac0d2d1764f37ea1f1eb49
X3ne <X3ne@users.noreply.github.com>
2024-02-17 11:17:13 +0100

    fix: change characters coords to improve text detection

--- src/config/config.go
+++ src/config/config.go
@@ -31,26 +31,26 @@ func (c *AppConfig) setCharactersCoords() {
 		c.CharactersCoords = []utils.Rect{
 			{X: 2250, Y: 305, Width: 170, Height: 30},
 			{X: 2250, Y: 400, Width: 170, Height: 30},
-			{X: 2250, Y: 490, Width: 170, Height: 30},
-			{X: 2250, Y: 590, Width: 170, Height: 30},
+			{X: 2250, Y: 495, Width: 170, Height: 30},
+			{X: 2250, Y: 585, Width: 170, Height: 30},
 		}
 		return
 	}
 	c.CharactersCoords = []utils.Rect{
-		{X: 1620, Y: 240, Width: 150, Height: 60},
-		{X: 1620, Y: 330, Width: 150, Height: 60},
-		{X: 1620, Y: 430, Width: 150, Height: 60},
-		{X: 1620, Y: 530, Width: 150, Height: 60},
+		{X: 1620, Y: 240, Width: 140, Height: 60},
+		{X: 1620, Y: 330, Width: 140, Height: 60},
+		{X: 1620, Y: 430, Width: 140, Height: 60},
+		{X: 1620, Y: 530, Width: 140, Height: 60},
 	}
 }
 
 func (c *AppConfig) setCharacterBoxCoords() {
 	if c.Ultrawide {
 		c.CharacterBoxCoords = []utils.Rect{
-			{X: 2400, Y: 351, Width: 200, Height: 1},
-			{X: 2400, Y: 445, Width: 200, Height: 1},
-			{X: 2400, Y: 538, Width: 200, Height: 1},
-			{X: 2400, Y: 632, Width: 200, Height: 1},
+			{X: 2400, Y: 351},
+			{X: 2400, Y: 445},
+			{X: 2400, Y: 538},
+			{X: 2400, Y: 632},
 		}
 		return
 	}
@@ -63,51 +63,21 @@ func (c *AppConfig) setCharacterBoxCoords() {
 }
 
 func (c *AppConfig) setLocationCoord() {
-	c.LocationCoord = utils.Rect{
-		X: 55,
-		Y: 15,
-		Width: 320,
-		Height: 25,
-	}
+	c.LocationCoord = utils.Rect{X: 55, Y: 15, Width: 320, Height: 25}
 }
 
 func (c *AppConfig) setGUICoords() {
 	if c.Ultrawide {
 		c.GUICoordsConfig = GUICoordsConfig{
-			EscCoord: utils.Rect{
-				X: 1925,
-				Y: 250,
-				Width: 180,
-				Height: 30,
-			},
-			MenusCoord: utils.Rect{
-				X: 100,
-				Y: 35,
-				Width: 300,
-				Height: 25,
-			},
-			CombatCoord: utils.Rect{
-				X: 2100,
-				Y: 25,
-				Width: 85,
-				Height: 40,
-			},
+			EscCoord: utils.Rect{X: 1925, Y: 250, Width: 180, Height: 30},
+			MenusCoord: utils.Rect{X: 100, Y: 35, Width: 300, Height: 25},
+			CombatCoord: utils.Rect{X: 2100, Y: 25, Width: 85, Height: 40},
 		}
 		return
 	}
 	c.GUICoordsConfig = GUICoordsConfig{
-		EscCoord: utils.Rect{
-			X: 0,
-			Y: 0,
-			Width: 0,
-			Height: 0,
-		},
-		MenusCoord: utils.Rect{
-			X: 0,
-			Y: 0,
-			Width: 0,
-			Height: 0,
-		},
+		EscCoord: utils.Rect{X: 0, Y: 0, Width: 0, Height: 0},
+		MenusCoord: utils.Rect{X: 0, Y: 0, Width: 0, Height: 0},
 	}
 }
 
